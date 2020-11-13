8c1ee56972c995a25f8f21b7ad9344fc3d8ca785
Sladrigezz <Sladrigezz@github.com>
2020-11-13 14:57:18 +0300

    api, prop-types, data-base

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import axios from 'axios'
 
 import {Header} from './components';
 import {Home, Cart} from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([])
+
+  React.useEffect(() => {
+    axios.get(`http://localhost:3000/db.json`)
+      .then(({data}) => {setPizzas(data.pizzas)})
+  }, [])
 
   return (
     <div className="wrapper">
@@ -13,7 +20,7 @@ function App() {
 
       <div className="content">
 
-        <Route path='/' exact component={Home}></Route>
+        <Route path='/' exact render={() => <Home items={pizzas}></Home>}></Route>
         <Route path='/cart' exact component={Cart}></Route>
 
       </div>
